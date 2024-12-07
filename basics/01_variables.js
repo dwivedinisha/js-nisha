a9b0bdee4cefb15f77086628f917563f10f9be23
Nisha Dwivedi <dwivedinisha975@gmail.com>
2024-12-07 19:37:00 +0000

    variables

---
+++ basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144553
+let accountEmail = "dwivedinisha248@gmail.com"
+var accountPassword = "12345"
+accountCity = "Banda"
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountCity,accountPassword])
+
