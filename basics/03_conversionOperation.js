13d9338308b824883e5938c42cbc581b5dffdaed
Nisha Dwivedi <dwivedinisha975@gmail.com>
2024-12-07 20:44:16 +0000

    con done

--- basics/03_conversionOperation.js
+++ basics/03_conversionOperation.js
@@ -5,3 +5,23 @@ let valueInVariable = Boolean(score);
 
 console.log(typeof valueInVariable);
 
+let str1 = "hello";
+let str2 = "nisha";
+console.log(str1+str2);
+console.log(1+2+"2");
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+
+
