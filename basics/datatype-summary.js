f825a478797de1215824422749c053974d22e1f2
Nisha Dwivedi <dwivedinisha975@gmail.com>
2024-12-08 07:23:38 +0000

    datatype done

---
+++ basics/datatype-summary.js
@@ -0,0 +1,28 @@
+//primitive
+
+// 7 types : string, number, boolean, null, unndefined, symbol, BigInt
+
+const score = 100;
+const scoreval = 103.3;
+
+console.log(typeof scoreval);
+
+//Reference (non-primitive)
+
+//Array, Objects,Functions
+
+const heros = ["shaktiman","naagraj","doga"];
+
+let myObj = {
+    name: "nisha",
+    age:18,
+}
+
+console.log(myObj);
+
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof myFunction);
