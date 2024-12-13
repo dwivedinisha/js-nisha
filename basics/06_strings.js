34f350c8689f17e4f11b5196a63e35e3c7b53602
Nisha Dwivedi <dwivedinisha975@gmail.com>
2024-12-13 04:33:11 +0000

    Array done

--- basics/06_strings.js
+++ basics/06_strings.js
@@ -3,31 +3,49 @@
 // console.log(`hello my name is ${name} and my age is${age}`);
 
 
-const name = new String("nisha");
+// const name = new String("nisha");
 
-console.log(name.charAt(2));
-console.log(name.indexOf('s'));
+// console.log(name.charAt(2));
+// console.log(name.indexOf('s'));
 
-const newString = name.slice(-5,2);
+// const newString = name.slice(-5,2);
 
-console.log(newString);
+// console.log(newString);
 
-//new type//
+// //new type//
 
-const str = "   radha  -   ";
-const  name2 = str.trim();
-console.log(str.length);
+// const str = "   radha  -   ";
+// const  name2 = str.trim();
+// console.log(str.length);
 
-console.log(name2.length);
+// console.log(name2.length);
 
-console.log(str.replace('-','dwivedi'));
+// console.log(str.replace('-','dwivedi'));
 
-console.log(str.trim());
+// console.log(str.trim());
 
-console.log(str.includes('dha'));
+// console.log(str.includes('dha'));
 
-const bigmouth = 'I\'ve got no right to take my place…';
-console.log(bigmouth);
+// const bigmouth = 'I\'ve got no right to take my place…';
+// console.log(bigmouth);
+
+
+//strings are immutable
+
+
+let str ="NishaDwivediNisha"
+//console.log(str.length);
+console.log(str.slice(1,3))//end is noo-exclusive
+console.log(str.concat(4577349858))
+console.log(str.replace("Nisha","Ishu"));
+console.log(str.replaceAll("Nisha","Ishu"));
+console.log(str.charAt(4));
+console.log();
+
+
+
+//console.log(str.trim().length);
+
 
 
 
---
+++ basics/array.js
@@ -0,0 +1,29 @@
+const arr = [3,5,6,true,"Nisha"]
+//console.log(typeof(arr));
+//console.log(arr[1]);
+//arr.push(3)
+// arr.pop()
+// arr.unshift(9)
+//arr.shift()
+
+// console.log(arr.includes(6));
+// console.log(arr.indexOf(19));//not found print -1
+// console.log(arr.indexOf(3));
+
+// const newarr = arr.join()//convert array into string 
+// console.log(newarr);
+// console.log(typeof(newarr));
+
+console.log("A",arr);
+console.log("B",arr.slice(0,4));//make a copy but do not hinder the original one
+console.log("c",arr);
+console.log("D",arr.splice(0,4));//changes the original string
+console.log("E",arr);//print the remaining original string after using splice
+
+
+
+
+
+
+
+
