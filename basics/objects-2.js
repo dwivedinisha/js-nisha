c811fafd79844b23a125578598662d2e0891f20c
Nisha Dwivedi <dwivedinisha975@gmail.com>
2024-12-17 18:15:23 +0000

    object-2  done

---
+++ basics/objects-2.js
@@ -0,0 +1,10 @@
+//const tinderuser = new Object();
+
+const tinderUser = {}
+tinderUser
+tinderUser.email = "nisha@google.com";
+tinderUser.name = "nisha dwivedi";
+
+//tinderUser.name = tinderUser["name"].toUpperCase();
+
+console.log(tinderUser);
